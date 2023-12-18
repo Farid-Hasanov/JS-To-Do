edd7ca010521cb16003a5357a4c2941d647aac82
Farid Hasanov <hasanovfarid021@gmail.com>
2023-12-18 21:00:19 +0000

    todo

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let z = a => {
+
+            for (let i = 1; i <= a; i++) {
+                $('.result').append(`<div class="minibox"><h2>${a}</h2></div>`);
+
+            }
+        }
+
+        let t = h => { h * 1 ? f($('input').val()) : $('.result').append(`<div class="minibox"><h2>${h}</h2></div>`);
+        }
+
+        t($('input').val())
+
+
+    });
+
+
 
 });
